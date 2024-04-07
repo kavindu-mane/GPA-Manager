ebffb427e3de118c4c2cbaf633a96e913c355718
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-04-08 02:00:13 +0530

    types update

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,10 @@ export type errorTypes = {
   name?: string[];
   repeat_password?: string[];
   message?: string;
+  semester?: string[];
+  subject?: string[];
+  grade?: string[];
+  credits?: string[];
 };
 
 //  user account data type
@@ -14,4 +18,35 @@ export type userDataType = {
   email: string;
   created_at?: string;
   updated_at?: string;
+};
+
+//  private routing object type
+export type privateRouteType = {
+  [key: string]: JSX.Element;
+};
+
+//semester response data type
+export type semesterDataType = {
+  id: string;
+  name: string;
+}[];
+
+// subject response data type
+export type subjectDataType = {
+  id: string;
+  name: string;
+  totalCredits: number;
+  gpa: number;
+  courses: {
+    id: number;
+    name: string;
+    grade: string;
+    gpa: number;
+    credits: number;
+  }[];
+};
+
+// table dropdown type
+export type tableDropDown = {
+  [key: number]: boolean;
 };
